8bb2097c893c5360b7c7c0eee2569043472359bd
؟MARTO <programmers.atlantis@gmail.com>
2026-09-20 20:51:16 +0330

    Update __init__.py

--- shadthon/__init__.py
+++ shadthon/__init__.py
@@ -1,35 +1,27 @@
 from .client import Client
-from .session import Session
-from .models import User, Message, Poll, FileInfo, LoginResult
+from .models import (
+    Message,
+    User,
+    Poll,
+    LoginResult,
+)
 from .exceptions import (
     ShadthonError,
     AuthenticationError,
-    InvalidAuthError,
     NetworkError,
     ProtocolError,
-    CryptoError,
-    SessionError,
-    UploadError,
-    DownloadError,
 )
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
-__all__ = [
+__all__ = (
     "Client",
-    "Session",
-    "User",
     "Message",
+    "User",
     "Poll",
-    "FileInfo",
     "LoginResult",
     "ShadthonError",
     "AuthenticationError",
-    "InvalidAuthError",
     "NetworkError",
     "ProtocolError",
-    "CryptoError",
-    "SessionError",
-    "UploadError",
-    "DownloadError",
-]
+)
