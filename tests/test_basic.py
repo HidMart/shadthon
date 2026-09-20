44e98fec12aa671d906e5e0e835d15d3145b883b
؟MARTO <programmers.atlantis@gmail.com>
2026-09-20 17:56:18 +0330

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,23 +1,46 @@
-from shadthon import Client, Session
+from shadthon import (
+    Client,
+    Message,
+    Poll,
+    User,
+)
 
 
-def test_session():
-    session = Session(
-        phone_number="09123456789"
-    )
-
-    assert session.phone_number == (
-        "09123456789"
-    )
-
-    assert not session.has_auth()
-
-
-def test_client():
+def test_import():
     client = Client(
         "09123456789"
     )
 
     assert client.phone_number == (
-        "09123456789"
+        "989123456789"
     )
+
+
+def test_models():
+    user = User.from_dict(
+        {
+            "user_guid": "u0-test",
+            "first_name": "Test",
+        }
+    )
+
+    assert user.guid == "u0-test"
+
+    message = Message.from_dict(
+        {
+            "message_id": "123",
+            "text": "Hello",
+        }
+    )
+
+    assert message.text == "Hello"
+
+    poll = Poll.from_dict(
+        {
+            "poll_id": "p1",
+            "question": "Test?",
+            "options": [],
+        }
+    )
+
+    assert poll.poll_id == "p1"
