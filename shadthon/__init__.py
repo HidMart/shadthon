bc645b15144eb4c317c8e5a0fb969d6f78ad21a3
؟MARTO <programmers.atlantis@gmail.com>
2026-09-20 21:40:38 +0330

    Update __init__.py

--- shadthon/__init__.py
+++ shadthon/__init__.py
@@ -1,27 +1,13 @@
 from .client import Client
-from .models import (
-    Message,
-    User,
-    Poll,
-    LoginResult,
-)
-from .exceptions import (
-    ShadthonError,
-    AuthenticationError,
-    NetworkError,
-    ProtocolError,
-)
+from .models import Message, User, LoginResult
+from .session import Session
 
 __version__ = "0.3.0"
 
-__all__ = (
+__all__ = [
     "Client",
     "Message",
     "User",
-    "Poll",
     "LoginResult",
-    "ShadthonError",
-    "AuthenticationError",
-    "NetworkError",
-    "ProtocolError",
-)
+    "Session",
+]
