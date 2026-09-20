dbfe1fdc2efd76002928844171c9db6ac2cd51bb
؟MARTO <programmers.atlantis@gmail.com>
2026-09-20 22:00:21 +0330

    Update __init__.py

--- shadthon/__init__.py
+++ shadthon/__init__.py
@@ -1,13 +1,14 @@
 from .client import Client
-from .models import Message, User, LoginResult
-from .session import Session
+from .models import Message, User, Chat
+from .exceptions import ShadthonError
 
-__version__ = "0.3.0"
+__version__ = "0.1.0"
 
 __all__ = [
     "Client",
     "Message",
     "User",
-    "LoginResult",
-    "Session",
+    "Chat",
+    "ShadthonError",
+    "__version__",
 ]
