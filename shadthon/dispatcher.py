910ccb09f40be15592cdd75947a4ebe7fe1f02b7
؟MARTO <programmers.atlantis@gmail.com>
2026-09-23 01:24:28 +0330

    Update dispatcher.py

--- shadthon/dispatcher.py
+++ shadthon/dispatcher.py
@@ -4,11 +4,7 @@ import inspect
 
 
 class Handler:
-    def __init__(
-        self,
-        callback,
-        filter_=None,
-    ):
+    def __init__(self, callback, filter_=None):
         self.callback = callback
         self.filter = filter_
 
@@ -16,9 +12,7 @@ class Handler:
         if self.filter is None:
             return True
 
-        return await self.filter.check(
-            message
-        )
+        return await self.filter.check(message)
 
     async def call(self, message):
         result = self.callback(message)
@@ -33,11 +27,7 @@ class Dispatcher:
     def __init__(self):
         self.handlers = []
 
-    def add_handler(
-        self,
-        callback,
-        filter_=None,
-    ):
+    def add_handler(self, callback, filter_=None):
         handler = Handler(
             callback,
             filter_,
@@ -55,15 +45,9 @@ class Dispatcher:
         ]
 
     async def dispatch(self, message):
-        for handler in list(
-            self.handlers
-        ):
+        for handler in list(self.handlers):
             try:
-                if await handler.matches(
-                    message
-                ):
-                    await handler.call(
-                        message
-                    )
+                if await handler.matches(message):
+                    await handler.call(message)
             except Exception:
                 continue
